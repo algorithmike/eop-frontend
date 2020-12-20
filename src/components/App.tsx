7dd5d6ca452e73ab84d6211024239307a03f446c
Michael Fernandez <mikevfern@gmail.com>
2020-12-20 11:39:47 -0800

    init material UI. test material UI

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,27 @@
+import { Button, Dialog, DialogTitle } from '@material-ui/core';
+import { Component } from 'react';
 import '../styles/App.scss';
 
-function App() {
-  return (
+class App extends Component {
+  
+  state = {
+    showDialog: false
+  }
+
+  render(){return (
     <div className="App">
-      Hello World!
+      <p>Hello World!</p>
+      <Button onClick={() => {
+        this.setState(() => {
+          console.log();
+          return {showDialog: !this.state.showDialog}
+        })
+      }}>Test</Button>
+      <Dialog open={this.state.showDialog}>
+        <DialogTitle>This is a test.</DialogTitle>
+      </Dialog>
     </div>
-  );
+  );}
 }
 
 export default App;
