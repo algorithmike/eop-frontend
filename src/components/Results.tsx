e7e1e1299c0718b4e9b72097ba50f03103b7b893
Michael Fernandez <mikevfern@gmail.com>
2021-01-30 20:36:00 -0800

    wiring up apollo client. init query in Results.tsx

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,11 +1,35 @@
-import { Component } from 'react';
+import { useQuery, gql } from '@apollo/client';
 
-export default class Results extends Component {
-  render() {
-    return (
-      <div>
-        <p>This is the page with search Results.</p>
-      </div>
-    );
+const CONTENT = gql`
+  query {
+    content {
+      id
+      title
+      mediaType
+      mediaUrl
+      mediaPreviewUrl
+      description
+    }
   }
+`;
+
+const Results = () => {
+  const { loading, error, data } = useQuery(CONTENT);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return (
+    <div>
+      {data.content.map(({title}: any, index: number) => {
+        return (
+          <div key={index}>
+            <h3>{title}</h3>
+          </div>
+        )
+      })}
+    </div>
+  );
 };
+
+export default Results;
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,23 @@
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 import App from './components/App';
 import instantiateStore from './store/store';
 
 const store = instantiateStore();
+const client = new ApolloClient({
+  uri: 'https://eop-backend.herokuapp.com/graphql',
+  cache: new InMemoryCache()
+});
+
+
 const JSX = (
+  <ApolloProvider client={client}>
     <Provider store={store}>
         <App />
     </Provider>
+  </ApolloProvider>
 );
 
 ReactDOM.render(JSX, document.getElementById('root'));
-
-store.subscribe(() => {
-  console.log('Current State: ', store.getState());
-});
