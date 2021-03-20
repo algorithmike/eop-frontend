6fd63c37294009eddf4d103cc2f02e1bc0008a4c
Michael Fernandez <mikevfern@gmail.com>
2021-03-20 15:41:58 -0700

    tests for filter actions and reducers

--- src/store/slices/tests/filter.test.js
+++ src/store/slices/tests/filter.test.js
@@ -1,4 +1,4 @@
-import { updateFilter, resetFilter } from '../filter';
+import filtersReducer, { updateFilter, resetFilter, initialFilterState } from '../filter';
 
 test('generate correct action with payload for updateFilter actionGenerator', () => {
     const updatedFilterMatcher = {
@@ -22,4 +22,34 @@ test('actionCreator creates correct action resetFilter actionGenerator', () => {
     const action = resetFilter()
 
     expect(action.type).toEqual('filters/resetFilter')
+})
+
+test('should setup initialFilterState', () => {
+    const state = filtersReducer(undefined, { type: '@@INIT' })
+    
+    expect(state).toEqual(initialFilterState)
+})
+
+test('dispatching updateFilter should update state for only text filter', () => {
+    const updatedFilterText = 'Updated filter text.'
+    const updatedState = filtersReducer(undefined, updateFilter({ text: updatedFilterText }))
+
+    expect(updatedState).toEqual({
+        ...initialFilterState,
+        text: updatedFilterText
+    })
+})
+
+test('dispatching resetFilter should update state to match initialFilterState', () => {
+    const updatedFilterText = 'Updated filter text.'
+    const updatedState = filtersReducer(undefined, updateFilter({ text: updatedFilterText }))
+
+    expect(updatedState).toEqual({
+        ...initialFilterState,
+        text: updatedFilterText
+    })
+
+    const resetState = filtersReducer(undefined, resetFilter())
+
+    expect(resetState).toEqual(initialFilterState)
 })
