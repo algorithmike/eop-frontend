828bc085b8c4bb77e4f2cbfc633c9744822e277b
Michael Fernandez <mikevfern@gmail.com>
2021-03-23 19:53:18 -0700

    Added snapshot test for Header

---
+++ src/components/__tests__/Header.test.jsx
@@ -0,0 +1,21 @@
+import { Provider } from 'react-redux';
+import { MuiPickersUtilsProvider } from '@material-ui/pickers';
+import renderer from 'react-test-renderer';
+import DayjsUtils from '@date-io/dayjs';
+
+import instantiateStore from '../../store/store';
+import Header from '../Header';
+
+test('should render header correctly', () => {
+    const store = instantiateStore();
+
+    const component = renderer.create(
+        <MuiPickersUtilsProvider utils={DayjsUtils}>
+            <Provider store={store}>
+                <Header />
+            </Provider>
+        </MuiPickersUtilsProvider>
+    );
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+})
