98fc0f6c41db7034d086f222a84b7179495e28dc
Michael Fernandez <mikevfern@gmail.com>
2021-03-27 18:23:25 -0700

    replace snapshot test. init testing-library/react

--- src/components/__tests__/Header.test.jsx
+++ src/components/__tests__/Header.test.jsx
@@ -1,22 +1,22 @@
 import { Provider } from 'react-redux';
+import { render, screen } from '@testing-library/react';
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
-import renderer from 'react-test-renderer';
 import DayjsUtils from '@date-io/dayjs';
 
 import instantiateStore from '../../store/store';
 import Header from '../Header';
 
-test('should render header correctly', () => {
+test('should render header with logo', () => {
     const store = instantiateStore();
 
-    const component = renderer.create(
+    render(
         <MuiPickersUtilsProvider utils={DayjsUtils}>
             <Provider store={store}>
                 <Header />
             </Provider>
         </MuiPickersUtilsProvider>
     );
-    const tree = component.toJSON();
-    console.log(tree)
-    expect(tree).toMatchSnapshot();
+
+    const eopLogo = screen.getByAltText(/eop logo/i);
+    expect(eopLogo).toBeInTheDocument;
 })
