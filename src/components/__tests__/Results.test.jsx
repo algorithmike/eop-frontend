b53c30252c7f12c1da84f9b363d33a7d5ae2c653
Michael Fernandez <mikevfern@gmail.com>
2021-03-29 12:40:11 -0700

    init Results 'Loading...' test

---
+++ src/components/__tests__/Results.test.jsx
@@ -0,0 +1,34 @@
+import { Provider } from 'react-redux';
+import { render, screen } from '@testing-library/react';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { createUploadLink } from 'apollo-upload-client';
+import { MuiPickersUtilsProvider } from '@material-ui/pickers';
+import DayjsUtils from '@date-io/dayjs';
+
+import instantiateStore from '../../store/store';
+import Results from '../Results';
+
+test('should render "Loading..." message', () => {
+    const store = instantiateStore();
+    const client = new ApolloClient({
+        link: createUploadLink({
+          uri: (process.env.NODE_ENV === 'production')
+            ? process.env.REACT_APP_API_URL
+            : 'http://localhost:4000/graphql'
+        }),
+        cache: new InMemoryCache()
+      });
+
+    render(
+        <ApolloProvider client={client}>
+            <MuiPickersUtilsProvider utils={DayjsUtils}>
+                <Provider store={store}>
+                    <Results />
+                </Provider>
+            </MuiPickersUtilsProvider>
+        </ApolloProvider>
+    );
+
+    const loadingMessage = screen.getByText('Loading...');
+    expect(loadingMessage).toBeInTheDocument();
+})
