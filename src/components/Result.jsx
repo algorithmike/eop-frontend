bba5e677eba7f0b01b0a6c9c94691632df6ec681
Michael Fernandez <mikevfern@gmail.com>
2021-04-05 17:48:20 -0700

    added modal

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,4 +1,9 @@
-import ReactPlayer from 'react-player'
+import { useState } from 'react';
+import ReactPlayer from 'react-player';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+
 import '../styles/Result.scss';
 
 const Result = ({data}) => {
@@ -6,8 +11,19 @@ const Result = ({data}) => {
     const date = new Date(createdAt);
     const dateString = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()} at ${date.getHours()}hrs and ${date.getMinutes()}mins`
 
+    const [imageViewOpen, setImageViewOpen] = useState(false);
+
+    const handleOpenImageModal = (imgUrl) => {
+        console.log(imgUrl);
+        setImageViewOpen(true);
+    };
+  
+    const handleCloseImageModal = () => {
+        setImageViewOpen(false);
+    };
+
     return (
-        <div className="result" >
+        <div className="result">
             {
                 (mediaType === 'video') &&
                 <ReactPlayer
@@ -18,7 +34,38 @@ const Result = ({data}) => {
                     previewTabIndex={5}
                 />
             }
-            {(mediaType === 'image') && <img className="content" src={mediaUrl} alt={title}/>}
+            {
+                (mediaType === 'image') && 
+                <>
+                    <img className="content"
+                        src={mediaUrl}
+                        alt={title}
+                        onClick={() => {
+                            handleOpenImageModal(mediaUrl);
+                        }}
+                    />
+                    <Modal
+                        className="modal"
+                        open={imageViewOpen}
+                        onClose={handleCloseImageModal}
+                        closeAfterTransition
+                        BackdropComponent={Backdrop}
+                        BackdropProps={{
+                            timeout: 800,
+                        }}
+                    >
+                        <Fade in={imageViewOpen}>
+                            <div onClick={handleCloseImageModal}>
+                                <img
+                                    src={mediaUrl}
+                                    alt={title}
+                                    size="100%"
+                                />
+                            </div>
+                        </Fade>
+                    </Modal>
+                </>
+            }
             <div className="result__text">
                 <h4>{title}</h4>
                 <p className="description">{description}</p>
