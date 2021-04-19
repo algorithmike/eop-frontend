ac6883448a7e24b195675d3100eca1b51cba028a
Michael Fernandez <mikevfern@gmail.com>
2021-04-19 11:33:12 -0700

    remove menu column. adjust layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,47 @@
-import { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Component } from "react";
+import { Switch, Route } from "react-router-dom";
 
-import '../styles/App.scss';
-import About from './About';
-import Header from './Header';
-import Help from './Help';
-import Login from './Login';
-import MenuColumn from './MenuColumn';
-import PageNotFound from './PageNotFound';
-import Results from './Results';
-import CreateContent from './CreateContent';
-
+import "../styles/App.scss";
+import About from "./About";
+import Header from "./Header";
+import Help from "./Help";
+import Login from "./Login";
+import MenuColumn from "./MenuColumn";
+import PageNotFound from "./PageNotFound";
+import Results from "./Results";
+import CreateContent from "./CreateContent";
 
 class App extends Component {
-  render(){return (
-    <div className="app">
-      <Header/>
-      <div className="app__main">
-        <MenuColumn/>
-        <Switch>
-          <Route path="/" exact={true}><Results/></Route>
-          <Route path="/about"><About/></Route>
-          <Route path="/help"><Help/></Route>
-          <Route path="/login"><Login/></Route>
-          <Route path ="/upload"><CreateContent/></Route>
-          <Route><PageNotFound/></Route>
-        </Switch>
+  render() {
+    return (
+      <div className="app">
+        <Header />
+        <div className="app__main">
+          {/* <MenuColumn/> */}
+          <Switch>
+            <Route path="/" exact={true}>
+              <Results />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/help">
+              <Help />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/upload">
+              <CreateContent />
+            </Route>
+            <Route>
+              <PageNotFound />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
-  );}
-};
+    );
+  }
+}
 
 export default App;
--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,81 +1,80 @@
-import { useState } from 'react';
-import ReactPlayer from 'react-player';
-import Modal from '@material-ui/core/Modal';
-import Backdrop from '@material-ui/core/Backdrop';
-import Fade from '@material-ui/core/Fade';
+import { useState } from "react";
+import ReactPlayer from "react-player";
+import Modal from "@material-ui/core/Modal";
+import Backdrop from "@material-ui/core/Backdrop";
+import Fade from "@material-ui/core/Fade";
 
-import '../styles/Result.scss';
+import "../styles/Result.scss";
 
-const Result = ({data}) => {
-    const {mediaType, mediaUrl, title, description, createdAt, event} = data;
-    const date = new Date(createdAt);
-    const dateString = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()} at ${date.getHours()}hrs and ${date.getMinutes()}mins`
+const Result = ({ data }) => {
+  const { mediaType, mediaUrl, title, description, createdAt, event } = data;
+  const date = new Date(createdAt);
+  const dateString = `${
+    date.getMonth() + 1
+  }/${date.getDate()}/${date.getFullYear()} at ${date.getHours()}hrs and ${date.getMinutes()}mins`;
 
-    const [imageViewOpen, setImageViewOpen] = useState(false);
+  const [imageViewOpen, setImageViewOpen] = useState(false);
 
-    const handleOpenImageModal = (imgUrl) => {
-        console.log(imgUrl);
-        setImageViewOpen(true);
-    };
-  
-    const handleCloseImageModal = () => {
-        setImageViewOpen(false);
-    };
+  const handleOpenImageModal = (imgUrl) => {
+    console.log(imgUrl);
+    setImageViewOpen(true);
+  };
 
-    return (
-        <div className="result">
-            {
-                (mediaType === 'video') &&
-                <ReactPlayer
-                    width="15rem"
-                    height="10rem"
-                    url={mediaUrl}
-                    controls={true}
-                    previewTabIndex={5}
-                />
-            }
-            {
-                (mediaType === 'image') && 
-                <>
-                    <img className="content"
-                        src={mediaUrl}
-                        alt={title}
-                        onClick={() => {
-                            handleOpenImageModal(mediaUrl);
-                        }}
-                    />
-                    <Modal
-                        className="modal"
-                        open={imageViewOpen}
-                        onClose={handleCloseImageModal}
-                        closeAfterTransition
-                        BackdropComponent={Backdrop}
-                        BackdropProps={{
-                            timeout: 800,
-                        }}
-                    >
-                        <Fade in={imageViewOpen}>
-                            <div onClick={handleCloseImageModal}>
-                                <img
-                                    src={mediaUrl}
-                                    alt={title}
-                                    size="100%"
-                                />
-                            </div>
-                        </Fade>
-                    </Modal>
-                </>
-            }
-            <div className="result__text">
-                <h4>{title}</h4>
-                <p className="description">{description}</p>
-                <p className="eventTitle">
-                    @<span className="emphasis">{event.title}</span> in {event.city}
-                </p>
-                <p className="dateString">{dateString}</p>
-            </div>
+  const handleCloseImageModal = () => {
+    setImageViewOpen(false);
+  };
+
+  return (
+    <div className="result">
+      {mediaType === "video" && (
+        <div className="video">
+          <ReactPlayer
+            width="18rem"
+            height="10rem"
+            url={mediaUrl}
+            controls={true}
+            previewTabIndex={5}
+          />
         </div>
-    )
-}
+      )}
+      {mediaType === "image" && (
+        <>
+          <img
+            className="content"
+            src={mediaUrl}
+            alt={title}
+            onClick={() => {
+              handleOpenImageModal(mediaUrl);
+            }}
+          />
+          <Modal
+            className="modal"
+            open={imageViewOpen}
+            onClose={handleCloseImageModal}
+            closeAfterTransition
+            BackdropComponent={Backdrop}
+            BackdropProps={{
+              timeout: 800,
+            }}
+          >
+            <Fade in={imageViewOpen}>
+              <div onClick={handleCloseImageModal}>
+                <img src={mediaUrl} alt={title} size="100%" />
+              </div>
+            </Fade>
+          </Modal>
+        </>
+      )}
+      <div className="result__text">
+        <h4>{title}</h4>
+        <p className="description">{description}</p>
+        <p className="eventTitle">
+          @<span className="emphasis">{event.title}</span> in {event.city}
+        </p>
+        <p className="dateString">{dateString}</p>
+      </div>
+    </div>
+  );
+};
 
-export default Result
+export default Result;
